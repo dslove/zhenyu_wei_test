ab650e9dd19bb816cd123db858ad2fe9a7ca3ad0
dslove <thomasweibravo@gmail.com>
2019-01-15 13:12:38 -0500

    Fixed a problem and added more test cases

--- question_two/version.py
+++ question_two/version.py
@@ -17,8 +17,45 @@ def compare(v1="", v2=""):
     if any([v1 == "", v2 == ""]):
         return 'One or both versions are not provided.'
 
-    list1 = list(v1)
-    list2 = list(v2)
+    characters1 = list(v1)
+    characters2 = list(v2)
+
+    if not characters1.index('.'):
+        return 'v1 is in wrong format'
+    if not characters2.index('.'):
+        return 'v2 is in wrong format'
+
+    def extract_number(characters):
+        working_list = []
+        resulting_list = []
+        dot_index = characters.index('.')
+        go_on = True
+        for i in range(dot_index):
+            if characters[i] == '0' and go_on:
+                continue
+            go_on = False
+            working_list.append(characters[i])
+        if not working_list:
+            working_list.append('0')
+        num_str = ''.join(working_list)
+        resulting_list.append(num_str)
+        resulting_list.append('.')
+        working_list.clear()
+        go_on = True
+        for i in range(len(characters)-(dot_index+1)):
+            index = i + (dot_index+1)
+            if characters[index] == '0' and go_on:
+                continue
+            go_on = False
+            working_list.append(characters[index])
+        if not working_list:
+            working_list.append('0')
+        num_str = ''.join(working_list)
+        resulting_list.append(num_str)
+        return resulting_list
+
+    list1 = extract_number(characters1)
+    list2 = extract_number(characters2)
 
     def check(a_list):
         if a_list[0].isdigit() and a_list[2].isdigit():
